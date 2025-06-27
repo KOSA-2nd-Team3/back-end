f25709e273437826059ea88bbe76b455dce6cb88
jaeyong <ljy3573057@gmail.com>
2025-06-27 16:20:59 +0900

    feat: 날짜 정렬 추가

--- src/main/java/kosa/server/board/repository/PostRepository.java
+++ src/main/java/kosa/server/board/repository/PostRepository.java
@@ -10,24 +10,25 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query(value = "SELECT p FROM Post p JOIN FETCH p.chatRoom cr WHERE p.member.id = :memberId", countQuery = "SELECT count (p) FROM Post p")
+    @Query(value = "SELECT p FROM Post p JOIN p.platform platform WHERE p.member.id = :memberId",
+            countQuery = "SELECT count(p) FROM Post p WHERE p.member.id = :memberId")
     Page<Post> findPostsByMemberId(Long memberId, Pageable pageable);
 
-    //PlatForm, Member 사용
-    Page<Post> findPostByPlatformName(String platformName, Pageable pageable);
-
-    List<Post> findByPlatformId(Long platformId);
-    
     @Query(value = "SELECT DISTINCT p FROM Post p " +
             "JOIN p.partyMember pm " +
             "JOIN pm.member m " +
+            "JOIN p.platform platform " +
             "WHERE m.loginId = :loginId " +
             "AND pm.isOwner = 'N'",
             countQuery = "SELECT COUNT(DISTINCT p) FROM Post p " +
                     "JOIN p.partyMember pm " +
                     "JOIN pm.member m " +
                     "WHERE m.loginId = :loginId " +
-                    "AND pm.isOwner = 'N'"
-    )
+                    "AND pm.isOwner = 'N'")
     Page<Post> findAllByPartyMemberLoginId(String loginId, Pageable pageable);
+
+    //PlatForm, Member 사용
+    Page<Post> findPostByPlatformName(String platformName, Pageable pageable);
+
+    List<Post> findByPlatformId(Long platformId);
 }
