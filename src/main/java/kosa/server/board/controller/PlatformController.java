51e26457c8c13d62a979ba16bf6ae64b92cb85ea
TW <teawon1005@naver.com>
2025-07-01 10:37:48 +0900

    refactor: 불필요한 코드 정리

--- src/main/java/kosa/server/board/controller/PlatformController.java
+++ src/main/java/kosa/server/board/controller/PlatformController.java
@@ -5,11 +5,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kosa.server.board.dto.request.SubscriptionCreateDto;
 import kosa.server.board.dto.response.*;
 import kosa.server.board.service.PlatformService;
-import kosa.server.board.service.PostService;
 import kosa.server.common.security.user.CustomUserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,7 +22,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PlatformController {
 
-    private final PostService postService;
     private final PlatformService platformService;
 
     // 메인 페이지
@@ -43,43 +40,19 @@ public class PlatformController {
         return new ResponseEntity<>(categoryPlatform, HttpStatus.OK);
     }
 
-    // 메인 페이지에서 플랫폼 하나 선택했을 때, 모든 방 리스트 출력
-    @Operation(summary = "플랫폼별 파티 목록 조회", description = "특정 플랫폼을 클릭했을 때 해당 플랫폼의 파티 목록을 조회합니다.")
-    @GetMapping("/{platformName}")
-    public ResponseEntity<Slice<PlatformPostResponseDto>> list(@PathVariable("platformName") String platformName) {
-        Slice<PlatformPostResponseDto> byPlatformName = postService.findByPlatformName(platformName);
-        return new ResponseEntity<>(byPlatformName, HttpStatus.OK);
+    // 플랫폼 별 파티 리스트 가져오기
+    @Operation(summary = "플랫폼별 파티 목록 조회", description = "특정 플랫폼의 모든 파티 리스트를 조회합니다.")
+    @GetMapping("/platform/{platformId}")
+    public ResponseEntity<List<PlatformPostResponseDto>> platformPostList(@PathVariable Long platformId) {
+        List<PlatformPostResponseDto> platformPostResponseDtos = platformService.platformPostList(platformId);
+        return new ResponseEntity<>(platformPostResponseDtos, HttpStatus.OK);
     }
 
-    // 플랫폼 기준 (현재인원 / 최대인원) 정렬
-    @Operation(summary = "현재 인원 비율로 정렬", description = "플랫폼 기준 비율로 정렬된 목록을 반환합니다.")
-    @GetMapping("/{platformName}/sort/rate")
-    public ResponseEntity<List<PlatformPeopleSortDto>> getStatsOrderByRate(@PathVariable("platformName") String platformName) {
-        List<PlatformPeopleSortDto> StatsOrderByRate = platformService.getStatsOrderByRate(platformName);
-        return new ResponseEntity<>(StatsOrderByRate, HttpStatus.OK);
-    }
-
-    // 메인 페이지에 있는 카테고리 버튼 클릭
-//    @GetMapping("/posts")
-//    public ResponseEntity<Slice<PlatformPostResponseDto>> listByCategory(@RequestParam int category, @RequestParam int page) {
-//        Slice<PlatformPostResponseDto> byCategory = platformService.findByCategory(category, page);
-//        return new ResponseEntity<>(byCategory, HttpStatus.OK);
-//    }
-
-    // 플랫폼 마다 구하는 글 개수
-    @Operation(summary = "플랫폼별 글 개수 정렬", description = "모든 플랫폼을 글 개수 기준으로 정렬하여 조회합니다.")
-    @GetMapping("/main/sort/count")
-    public ResponseEntity<List<PlatformSortResponseDto>> getStatsOrderByCount() {
-        List<PlatformSortResponseDto> StatsOrderByCount = platformService.getStatsOrderByCount();
-        return new ResponseEntity<>(StatsOrderByCount, HttpStatus.OK);
-    }
-
-    // 가격 별로
-    @Operation(summary = "가격별 플랫폼 정렬", description = "모든 플랫폼을 가격 기준으로 정렬하여 조회합니다.")
-    @GetMapping("/main/sort/price")
-    public ResponseEntity<List<PlatformSortResponseDto>> getPriceByPlatform() {
-        List<PlatformSortResponseDto> PriceByPlatform = platformService.getStatusOrderByPrice();
-        return new ResponseEntity<>(PriceByPlatform, HttpStatus.OK);
+    @Operation(summary = "플랫폼별 파티 통계 조회", description = "특정 플랫폼에 대해 빈 파티 정보를 조회합니다.")
+    @GetMapping("/platform/{platformId}/")
+    public ResponseEntity<PlatformPostNullResponseDto> platformPostNull(@PathVariable Long platformId) {
+        PlatformPostNullResponseDto platformPostNulls = platformService.platformPostNull(platformId);
+        return new ResponseEntity<>(platformPostNulls, HttpStatus.OK);
     }
 
     @Operation(summary = "내 구독 플랫폼 조회", description = "현재 로그인한 사용자의 구독 중인 플랫폼 목록을 조회합니다.")
--- src/main/java/kosa/server/board/service/PlatformService.java
+++ src/main/java/kosa/server/board/service/PlatformService.java
@@ -5,13 +5,11 @@ import kosa.server.board.entity.Platform;
 import kosa.server.board.entity.Post;
 import kosa.server.board.repository.PlatformRepository;
 import kosa.server.board.repository.PostRepository;
+import kosa.server.common.code.ErrorCode;
+import kosa.server.member.exception.InvalidArgumentException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -52,6 +50,34 @@ public class PlatformService {
                 .collect(Collectors.toList());
     }
 
+    public List<PlatformPostResponseDto> platformPostList(Long platformId) {
+        List<Post> findPostList = postRepository.findByPlatformId(platformId);
+
+        return findPostList.stream().map(post -> PlatformPostResponseDto.builder()
+                .postId(post.getId())
+                .leaderName(post.getMember().getName())
+                .platformName(post.getPlatform().getName())
+                .platformPrice(post.getPlatform().getPrice().longValue())
+                .monthUnit(post.getPlatform().getMonthUnit())
+                .currentCount(post.getCurrentCount())
+                .partySize(post.getPartySize())
+                .durationMonth(post.getDurationMonth())
+                .isExpired(post.getIsExpired())
+                .startDate(post.getStartDate())
+                .createdAt(post.getCreatedAt())
+                .build()).toList();
+    }
+
+    public PlatformPostNullResponseDto platformPostNull(Long platformId) {
+        Platform platform = platformRepository.findById(platformId)
+                .orElseThrow(()->new InvalidArgumentException(ErrorCode.MEMBER_NOT_FOUND));
+
+        return PlatformPostNullResponseDto.builder()
+                .platformName(platform.getName())
+                .platformPrice(platform.getPrice())
+                .build();
+    }
+
     public List<PlatformResponseDto> getPlatformList() {
         return platformRepository.findAll().stream().map(p -> PlatformResponseDto.builder()
                 .platformId(p.getId())
@@ -72,87 +98,4 @@ public class PlatformService {
                 .monthUnit(findPlatform.getMonthUnit())
                 .build();
     }
-
-    // 구하는 글 개수
-    public List<PlatformSortResponseDto> getStatsOrderByCount() {
-        List<PlatformSortResponseDto> list = getStatsCommon();
-        list.sort(Comparator.comparing(PlatformSortResponseDto::getPostCount).reversed());
-        return list;
-    }
-
-    // 가격
-    public List<PlatformSortResponseDto> getStatusOrderByPrice() {
-        List<PlatformSortResponseDto> list = getStatsCommon();
-        list.sort(Comparator.comparing(PlatformSortResponseDto::getOnePersonPrice));
-        return list;
-    }
-
-
-    private List<PlatformSortResponseDto> getStatsCommon() {
-        List<Object[]> results = platformRepository.countPostStatsByPlatform();
-        List<PlatformSortResponseDto> dtos = new ArrayList<>();
-        for(Object[] row : results) {
-            String name = (String) row[0];
-            Long postCount = (Long) row[1];
-            BigDecimal price = (BigDecimal) row[2];
-            int capacity = (Integer) row[3];
-            BigDecimal onePersonPrice = price.divide(BigDecimal.valueOf(capacity), 0);
-            dtos.add(new PlatformSortResponseDto(name, postCount, onePersonPrice, capacity));
-        }
-        return dtos;
-    }
-
-    // 플랫폼 선택 후 현재 인원 정렬
-    public List<PlatformPeopleSortDto> getStatsOrderByRate(String platformName) {
-        List<Object[]> results = platformRepository.findPostStatsWithPlatform(platformName);
-        List<PlatformPeopleSortDto> list = new ArrayList<>();
-        for (Object[] row : results) {
-            Long postId = ((Number) row[0]).longValue();
-            String name = (String) row[1];
-            String memberId = String.valueOf(row[2]);  // 가입자 ID (숫자일 수도 있으니 String 변환)
-            BigDecimal price = (BigDecimal) row[3];
-            int partySize = ((Number) row[4]).intValue();
-            int currentCount = ((Number) row[5]).intValue();
-            BigDecimal onePersonPrice = price.divide(BigDecimal.valueOf(partySize), 0, BigDecimal.ROUND_HALF_UP);
-            int leftSeat = partySize - currentCount;
-            double rate = (double) currentCount / partySize;
-            list.add(new PlatformPeopleSortDto(postId, platformName, memberId, onePersonPrice, partySize, currentCount, leftSeat, rate));
-        }
-        list.sort(Comparator.comparing(PlatformPeopleSortDto::getRate).reversed());
-        return list;
-    }
-
-    /*private List<PlatformSortResponseDto> getStatsCommon() {
-        List<Object[]> results = platformRepository.countPostStatsByPlatform();
-        List<PlatformSortResponseDto> dtos = new ArrayList<>();
-        for(Object[] row : results) {
-            String name = (String) row[0];
-            Long postId = (Long) row[1];
-            BigDecimal price = (BigDecimal) row[2];
-            int currentCount = row[3] != null ? ((Long)row[3]).intValue() : 0;
-            int maxCount = row[4] != null ? ((Long)row[4]).intValue() : 0;
-            BigDecimal onePersonPrice = price.divide(BigDecimal.valueOf(maxCount), 0);
-            double rate = (double) currentCount / maxCount;
-            dtos.add(new PlatformSortResponseDto(name, postId, onePersonPrice, maxCount, currentCount , rate));
-        }
-        return dtos;
-    }*/
-
-
-
-
-
-    /*public Slice<PlatformPostResponseDto> findByCategory(int category, int page) {
-        PageRequest pageRequest = PageRequest.of(page, 10, Sort.by("platform.price").descending());
-        Slice<Post> postByPlatformName = platformRepository.findByCategory(category, pageRequest);
-
-        return postByPlatformName.map(post -> PlatformPostResponseDto.builder()
-                .platformName(post.getPlatform().getName())
-                .partySize(post.getPartySize())
-                .currentCount(post.getCurrentCount())
-                .memberName(post.getMember().getName())
-                .postId(post.getPostId())
-                .isExpired(post.getIsExpired())
-                .build());
-    }*/
 }
